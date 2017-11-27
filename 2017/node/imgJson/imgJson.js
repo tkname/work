21ab37e47b9dd46afdee72c07154c48ce756871a
Areca <1250524789@qq.com>
2017-11-27 19:25:14 +0800

    fileChace

--- 2017/node/imgJson/imgJson.js
+++ 2017/node/imgJson/imgJson.js
@@ -3,6 +3,9 @@ var fs = require("fs");
 var obj={};
 	obj.fileArray=[];
 
+var date=new Date();
+var getTime=date.getTime();
+
 
 // 	 var jsonObj = JSON.parse(data); //字符串转json
 // 	fs.writeFileSync('./test.json', JSON.stringify(obj));  //josn转字符串
@@ -18,15 +21,15 @@ function ecchDir(path){
 					var oArray=file.split(".");	
 					var o={};
 					o.id=oArray[0];
-					o.src="../images/"+file;
+					o.src=path+file+"?v="+getTime;
 					obj.fileArray.push(o);	
-					console.log(JSON.stringify(obj)+"r\n");
-					fs.writeFileSync('./data.json', JSON.stringify(obj)+"\n");
-					console.log("正在生成"+file);
+					// console.log(JSON.stringify(obj)+"r\n");
+					fs.writeFileSync('js/data.json', JSON.stringify(obj.fileArray));
+					console.log("正在生成"+path+file+"\n");
 				 }
 			}
 		})
 	})
 })
 }
-ecchDir("./");
+ecchDir("./images/");
